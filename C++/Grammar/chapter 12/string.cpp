a055deb18ce128928863315882c2be13463ba788
Qfeel <qfeel0812@gmail.com>
2021-11-15 18:40:15 +0900

    CPP string replasce

---
+++ C++/Grammar/chapter 12/string.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstring>
+
+
+using namespace std;
+
+class String{
+	private:
+		char *txt;
+	public:
+		String(char *str)
+		{
+			
+		
+		}
+
+
+};
+
+
+
+int main(void)
+{
+
+
+
+}
