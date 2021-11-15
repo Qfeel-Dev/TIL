db4ac0b34ce843faf34ce80abdf5d772dedcf75b
Qfeel <qfeel0812@gmail.com>
2021-11-15 23:17:17 +0900

    CPP string replace class make

--- C++/Grammar/chapter 12/string.cpp
+++ C++/Grammar/chapter 12/string.cpp
@@ -8,10 +8,29 @@ class String{
 	private:
 		char *txt;
 	public:
-		String(char *str)
-		{
-			
 		
+	
+		
+		String(const char *str)
+		{
+			txt = new char[strlen(str)+1];
+			strcpy(txt,str);			
+		}
+		
+		String operator=(char* str)
+		{
+			delete []txt;
+			txt = new char[strlen(str)+1];
+			strcpy(txt,str);			
+		}
+	
+		
+		
+		
+		
+		void Show() const
+		{
+			cout << txt << endl;
 		}
 
 
@@ -21,7 +40,15 @@ class String{
 
 int main(void)
 {
-
-
+	String str1("I like a girl");
+	String str2 = "hello";
+	String str3();
+	
+	str2 = "I love girl";
+	str3 ="I love";
+	
+	str1.Show();
+	str2.Show();
+	
 
 }
