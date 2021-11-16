1c4ed649a7961ae2e197cb746d26e2796d06e3d4
Qfeel <qfeel0812@gmail.com>
2021-11-16 23:14:05 +0900

    Cpp string replace class

--- C++/Grammar/chapter 12/string.cpp
+++ C++/Grammar/chapter 12/string.cpp
@@ -7,27 +7,61 @@ using namespace std;
 class String{
 	private:
 		char *txt;
+		//static int num;
+		//int id;
 	public:
 		
 	
 		
-		String(const char *str)
+		String(const char *s)
 		{
-			txt = new char[strlen(str)+1];
-			strcpy(txt,str);			
+			//id = num;
+			//num++; 
+			//cout << id <<"Constructor" << endl;
+			txt = new char[strlen(s)+1];
+			strcpy(txt,s);			
 		}
 		
-		String operator=(char* str)
+		
+		String(const String& s)
 		{
+			//id = num;
+			//num++; 
+			//cout << id <<"Copy Constructor" << endl;
+			txt = new char[strlen(s.txt)+1];
+			strcpy(txt,s.txt);			
+		}
+		
+		
+		String operator=(const char* s)
+		{
+			//cout <<id << "= overloading" << endl;
 			delete []txt;
-			txt = new char[strlen(str)+1];
-			strcpy(txt,str);			
+			txt = new char[strlen(s)+1];
+			strcpy(txt,s);			
 		}
 	
+		String operator=(const String& s)
+		{
+			//cout <<id << "= overloading string" << endl;
+			delete []txt;
+			txt = new char[strlen(s.txt)+1];
+			strcpy(txt,s.txt);			
+		}
 		
 		
+		String operator+(String& s)
+		{
+			char *txt;
+			txt = new char[strlen(s.txt) + strlen(this->txt) + 1];
+			strcpy(txt,this->txt);
+			strcat(txt,s.txt);
+			
+			String tmp(txt);
+			return tmp;
 		
-		
+		}
+	
 		void Show() const
 		{
 			cout << txt << endl;
@@ -36,19 +70,25 @@ class String{
 
 };
 
+//int String::num = 0;
+
 
 
 int main(void)
 {
 	String str1("I like a girl");
 	String str2 = "hello";
-	String str3();
+	String str3 = str2;
+
 	
 	str2 = "I love girl";
-	str3 ="I love";
+	
+	String str4 = str1 + str2;	
 	
 	str1.Show();
 	str2.Show();
+	str3.Show();
+	str4.Show();
 	
 
 }
