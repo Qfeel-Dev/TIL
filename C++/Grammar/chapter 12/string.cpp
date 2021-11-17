ca87945a19aaf219e441b46713e979a2af593094
Qfeel <qfeel0812@gmail.com>
2021-11-17 21:55:31 +0900

    Cpp string class replace finish

--- C++/Grammar/chapter 12/string.cpp
+++ C++/Grammar/chapter 12/string.cpp
@@ -7,17 +7,11 @@ using namespace std;
 class String{
 	private:
 		char *txt;
-		//static int num;
-		//int id;
-	public:
-		
 	
-		
+	public:
+				
 		String(const char *s)
 		{
-			//id = num;
-			//num++; 
-			//cout << id <<"Constructor" << endl;
 			txt = new char[strlen(s)+1];
 			strcpy(txt,s);			
 		}
@@ -25,9 +19,6 @@ class String{
 		
 		String(const String& s)
 		{
-			//id = num;
-			//num++; 
-			//cout << id <<"Copy Constructor" << endl;
 			txt = new char[strlen(s.txt)+1];
 			strcpy(txt,s.txt);			
 		}
@@ -35,7 +26,6 @@ class String{
 		
 		String operator=(const char* s)
 		{
-			//cout <<id << "= overloading" << endl;
 			delete []txt;
 			txt = new char[strlen(s)+1];
 			strcpy(txt,s);			
@@ -43,14 +33,13 @@ class String{
 	
 		String operator=(const String& s)
 		{
-			//cout <<id << "= overloading string" << endl;
 			delete []txt;
 			txt = new char[strlen(s.txt)+1];
 			strcpy(txt,s.txt);			
 		}
 		
 		
-		String operator+(String& s)
+		String operator+(const String& s)
 		{
 			char *txt;
 			txt = new char[strlen(s.txt) + strlen(this->txt) + 1];
@@ -62,6 +51,56 @@ class String{
 		
 		}
 	
+		String operator+=(const String& s)
+		{
+			char *txt;
+			txt = new char[strlen(s.txt) + strlen(this->txt) + 1];
+			strcpy(txt,this->txt);
+			strcat(txt,s.txt);
+			
+			delete []this->txt;
+			this->txt = new char[strlen(s.txt) + strlen(this->txt) + 1];
+			strcpy(this->txt,txt);
+						
+			delete []txt;
+		}
+	
+		friend bool operator==(const String& str1 , const String& str2)
+		{
+			cout << "== overloading" << endl;
+			
+			if(strlen(str1.txt) != strlen(str2.txt))
+			{
+				cout << "lenth false" << endl;
+				cout << str1.txt << " || "<<str2.txt << endl;
+			
+				 return false;
+			
+			}
+			for(int i=0; i<strlen(str1.txt); i++)
+			{
+			
+				if(str1.txt[i] != str2.txt[i]) return false;			
+			}
+			
+			return true;
+		}
+		
+		friend ostream& operator<<(ostream& os, const String& s)
+		{
+			os << s.txt;
+			return os;
+			
+		}
+
+		friend istream& operator<<(istream& is, String& s)
+		{
+			char txt[100];
+			is >> txt;
+			s = String(txt);
+			return is;
+		}	
+	
 		void Show() const
 		{
 			cout << txt << endl;
@@ -70,25 +109,29 @@ class String{
 
 };
 
-//int String::num = 0;
-
-
 
 int main(void)
 {
-	String str1("I like a girl");
+	String str1("I love girl");
 	String str2 = "hello";
-	String str3 = str2;
 
-	
-	str2 = "I love girl";
-	
-	String str4 = str1 + str2;	
-	
+	str1 = "I love girl";
+	str2 = "I love gira";
+		
 	str1.Show();
 	str2.Show();
-	str3.Show();
-	str4.Show();
+
+	if(str1 == str2)
+	{
+		cout << "right" << endl;
+	}
 	
+	else
+	{
+		cout << "wrong" << endl;
+	}
+	
+	cout<<str1<<endl;
+	 
 
 }
