fbce1a0736c6145827bcfd3431f222f47f893c3e
Qfeel <qfeel0812@gmail.com>
2022-01-23 23:35:14 +0900

    Codingtest Doubly Linked List

--- C++/Baekjoon/ALDS1_3_C Doubly Linked List/ALDS1_3_C Doubly Linked List.cpp
+++ C++/Baekjoon/ALDS1_3_C Doubly Linked List/ALDS1_3_C Doubly Linked List.cpp
@@ -1,43 +1,99 @@
 #include <iostream>
 #include <cstring>
+#include <cstdlib>
 
 
 struct Node
 {
 	int key;
 	Node *prev, *next;
+};
+
+Node *nil;
+
+Node* listSearch(int key){
+	Node *cur = nil->next;
+	while(cur != nil && cur->key != key){
+		cur = cur -> next;
+	}
+	return cur;
 }
+
+void init()
+{
+	nil = (Node *)malloc(sizeof(Node));
+	nil->next = nil;
+	nil->prev = nil;
+}
+
+void printList(){
+	Node *cur = nil->next;
+	int isf = 0;
+	while(1){
+		if(cur == nil) break;
+		if(isf++ > 0 ) printf(" ");
+		printf("%d",cur->key);
+		cur = cur->next;
+	}
+	printf("\n");
+}
+
+void deleteNode(Node *t){
+	if(t == nil) return;
+	t->prev->next = t->next;
+	t->next->prev = t->prev;
+	free(t);
+}
+
+void deleteFirst(){
+	deleteNode(nil->next);
+}
+
+void deleteLast(){
+	deleteNode(nil->prev);
+}
+
+void deleteKey(int key){
+	deleteNode(listSearch(key));
+}
+
+void insert(int key){
+	Node *x = (Node *)malloc(sizeof(Node));
+	x->key = key;
+	x->next = nil->next;
+	nil->next->prev = x;
+	nil->next = x;
+ 	x->prev = nil;
+}
+
 
 
 
 int main()
 {
-	int command_num;
-	char command[15];
-
-	scanf("%d",&command_num);
+	int key,n,i;
+	int size=0;
+	char com[20];
+	int np=0,nd=0;
 	
-	for(int i = 0 ; i< command_num; i++)
-	{
-		scanf("%s",command);
-		if(command[0] == 'i'){}
-		else
-		{
-			if(strlen(command) == 11){//delete first
-			
+	scanf("%d",&n);
+	init();
+	for(int i=0; i<n; i++){
+		scanf("%s%d",com,&key);
+		if(com[0]=='i') {insert(key); np++; size++;}
+		else if(com[0] == 'd'){
+			if(strlen(com) > 6){
+				if(com[6] == 'F') deleteFirst();
+				else deleteLast();
 			}
-
-			else if(strlen(command) == 10){//delete last
-			
+			else{
+				deleteKey(key); nd++;
 			}
-
-			else{//delete x
-			
-			}
-
+			size++;
 		}
-	
 	}
 
+	printList();
 
+	
 }
