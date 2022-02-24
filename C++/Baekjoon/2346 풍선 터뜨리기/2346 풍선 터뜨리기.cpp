cbb745192580e3ab0939c964771dbebcb52f921b
Qfeel <qfeel0812@gmail.com>
2022-02-24 23:28:41 +0900

    Baekjoon 2346 풍선 터뜨리기

---
+++ C++/Baekjoon/2346 풍선 터뜨리기/2346 풍선 터뜨리기.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+struct t{
+	int arr;
+	int value;
+};
+
+
+int main(){
+	list <int> S;
+	int num;
+	int tmp;
+	int lim;
+	int pt = 1;
+
+	cin >> num;
+	for(int i = 0; i<num; i++){
+		cin >> tmp;
+		S.push_back(tmp);
+	}
+
+	lim = num;
+	int ans[num];
+
+
+	while(true){
+			
+
+
+	}
+}
